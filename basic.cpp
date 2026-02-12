6334f9b6368591ad8dc1fd95aed87d939b458105
Nisha R S <nisha.s@s.amity.edu>
2026-02-12 23:12:47 +0530

    made text changes

--- basic.cpp
+++ basic.cpp
@@ -4,8 +4,8 @@ int main(){
   int a=2;
   int b=a++;
     cout<<a<<"Post-Increment"<<b<<endl; //first work & then increment
-    int a=2;
-  int b=++a;
-    cout<<a<<"Pre-Increment"<<b<<endl; //first increment & then work
+    int c=2;
+  int d=++c;
+    cout<<c<<"Pre-Increment"<<d<<endl; //first increment & then work
 return 0;
 }
