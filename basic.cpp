9001b1349b0f687ba599753431e8c0013d4860e9
Nisha R S <nisha.s@s.amity.edu>
2026-02-12 23:17:50 +0530

    learnt pre & post increment

--- basic.cpp
+++ basic.cpp
@@ -3,9 +3,9 @@ using namespace std;
 int main(){
   int a=2;
   int b=a++;
-    cout<<a<<"Post-Increment"<<b<<endl; //first work & then increment
+    cout<<"Post-Increment"<<b<<endl; //first work & then increment
     int c=2;
   int d=++c;
-    cout<<c<<"Pre-Increment"<<d<<endl; //first increment & then work
+    cout<<"Pre-Increment"<<d<<endl; //first increment & then work
 return 0;
 }
