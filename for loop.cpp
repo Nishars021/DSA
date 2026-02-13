2c75d4c4dab753bd7f39f8214df495fea02afb33
Nisha R S <nisha.s@s.amity.edu>
2026-02-13 21:50:56 +0530

    performed for loop adding numbers

--- for loop.cpp
+++ for loop.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int i;
-    for(i=1;i<=3;i++){
-        cout<<i;
+    int i,n;
+    int sum=0;
+    cout<<"Enter the value of n: ";
+    cin>>n;
+    for(i=0;i<=n;i++){
+        sum=sum+i;
     }
+    cout<<"Sum : "<<sum;
     return 0;
 }
