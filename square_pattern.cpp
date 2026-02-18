07d394cad4186ff316b7caf2e6ce946e3c6cefd6
Nisha R S <nisha.s@s.amity.edu>
2026-02-18 13:44:40 +0530

    Saving my latest work

--- square_pattern.cpp
+++ square_pattern.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 int main (){
     int i,j;
-    int n=5;
-    // cout<<"Enter n value : ";
-    // cin>>n;
+    int n;
+    cout<<"Enter n value : ";
+    cin>>n;
     for(int i=1;i<=n;i++){
         for(int j=1;j<=n;j++){
             cout<<j <<" ";
