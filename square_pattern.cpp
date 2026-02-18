08716d5d5c5dfa9dc5b775d476263b5623bcdb11
Nisha R S <nisha.s@s.amity.edu>
2026-02-18 13:25:08 +0530

    added

--- square_pattern.cpp
+++ square_pattern.cpp
@@ -6,9 +6,8 @@ int main (){
     cout<<"Enter n value : ";
     cin>>n;
     for(int i=1;i<=n;i++){
-        cout<<"* "<<endl;
         for(int j=1;j<=i;j++){
-            cout<<"* ";
+            cout<<j;
         }
     }
     return 0;
