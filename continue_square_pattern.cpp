87038e79774aef593fdd9cc9405aa99bf4e586d6
Nisha R S <nisha.s@s.amity.edu>
2026-02-20 09:01:58 +0530

    performing square pattern for alphabets

--- continue_square_pattern.cpp
+++ continue_square_pattern.cpp
@@ -1,18 +1,20 @@
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
-int main(){
-   int i,j;
-   int n=2;
-   int a=1;
-   for(int i=0;i<n;i++){
-    for(int j=0;j<n;j++){
+// int main(){
+//    int i,j;
+//    int n=2;
+//    int a=1;
+//    for(int i=0;i<n;i++){
+//     for(int j=0;j<n;j++){
         
-        cout<<a<<" ";
-        a++;
-    }
-    cout<<endl;
-   }
-   cout<<"The last number after the pattern : "<<a;
-   return 0;
-}
+//         cout<<a<<" ";
+//         a++;
+//     }
+//     cout<<endl;
+//    }
+//    cout<<"The last number after the pattern : "<<a;
+//    return 0;
+// }
+
+#inc
