e0303adcddc73261c8ff795665771bd19bf12139
Nisha R S <nisha.s@s.amity.edu>
2026-02-20 09:21:57 +0530

    performed for numbers

--- continue_square_pattern.cpp
+++ continue_square_pattern.cpp
@@ -1,37 +1,39 @@
-// #include <iostream>
-// using namespace std;
-
-// int main(){
-//    int i,j;
-//    int n=2;
-//    int a=1;
-//    for(int i=0;i<n;i++){
-//     for(int j=0;j<n;j++){
-        
-//         cout<<a<<" ";
-//         a++;
-//     }
-//     cout<<endl;
-//    }
-//    cout<<"The last number after the pattern : "<<a;
-//    return 0;
-// }
-
 #include <iostream>
 using namespace std;
 
 int main(){
-    int i,j;
-     int n;
-     cout<<"Enter n value : ";
-     cin>>n;
-     char ch='A';
-     for(int i=1;i<=n;i++){
-      for(int j=1;j<=n;j++){
-                 cout<<ch<<" ";
-                 ch=ch+1;
-            }
-            cout<<endl;
-      }
-      return 0;
+   int i,j;
+   int n;
+   cout<<"Enter n value : ";
+   cin>>n;
+   int a=1;
+   for(int i=0;i<n;i++){
+    for(int j=0;j<n;j++){
+        
+        cout<<a<<" ";
+        a++;
+    }
+    cout<<endl;
+   }
+   cout<<"The last number after the pattern : "<<a;
+   return 0;
 }
+
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     int i,j;
+//      int n;
+//      cout<<"Enter n value : ";
+//      cin>>n;
+//      char ch='A';
+//      for(int i=1;i<=n;i++){
+//       for(int j=1;j<=n;j++){
+//                  cout<<ch<<" ";
+//                  ch=ch+1;
+//             }
+//             cout<<endl;
+//       }
+//       return 0;
+// }
