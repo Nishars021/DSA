0362a4812d71fa4a2721c5f726564dd9d1786288
Nisha R S <nisha.s@s.amity.edu>
2026-02-20 09:14:34 +0530

    added few lines

--- continue_square_pattern.cpp
+++ continue_square_pattern.cpp
@@ -26,8 +26,8 @@ int main(){
      cout<<"Enter n value : ";
      cin>>n;
      char ch='A';
-     for(int i=0;i<n;i++){
-      for(int j=0;j<n;j++){
+     for(int i=1;i<=n;i++){
+      for(int j=1;j<=n;j++){
                  cout<<ch<<" ";
                  ch=ch+1;
             }
