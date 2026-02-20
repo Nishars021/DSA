531b0da193af3cbf406528bb22aa1394379f85bc
Nisha R S <nisha.s@s.amity.edu>
2026-02-20 09:44:46 +0530

    added both numbers & alphabet code in 1 file.

--- continue_square_pattern.cpp
+++ continue_square_pattern.cpp
@@ -1,38 +1,38 @@
-#include <iostream>
-using namespace std;
-
-int main(){
-   int i,j;
-   int n;
-   cout<<"Enter n value : ";
-   cin>>n;
-   char ch='A';
-   for(int i=1;i<n;i++){
-    for(int j=1;j<n;j++){
-        
-        cout<<ch<<" ";
-        ch++;
-    }
-    cout<<endl;
-   }
-   return 0;
-}
-
 // #include <iostream>
 // using namespace std;
 
 // int main(){
-//     int i,j;
-//      int n;
-//      cout<<"Enter n value : ";
-//      cin>>n;
-//      char ch='A';
-//      for(int i=1;i<=n;i++){
-//       for(int j=1;j<=n;j++){
-//                  cout<<ch<<" ";
-//                  ch=ch+1;
-//             }
-//             cout<<endl;
-//       }
-//       return 0;
+//    int i,j;
+//    int n;
+//    cout<<"Enter n value : ";
+//    cin>>n;
+//    int a=1;
+//    for(int i=0;i<n;i++){
+//     for(int j=0;j<n;j++){
+        
+//         cout<<a<<" ";
+//         a++;
+//     }
+//     cout<<endl;
+//    }
+//    return 0;
 // }
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int i,j;
+     int n;
+     cout<<"Enter n value : ";
+     cin>>n;
+     char ch='A';
+     for(int i=0;i<n;i++){
+      for(int j=0;j<n;j++){
+                 cout<<ch<<" ";
+                 ch++;
+            }
+            cout<<endl;
+      }
+      return 0;
+}
