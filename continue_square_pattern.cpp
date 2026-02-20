cca23f843576eaa1fcfc6ad8827d4efce44371f3
Nisha R S <nisha.s@s.amity.edu>
2026-02-20 09:09:55 +0530

    added

--- continue_square_pattern.cpp
+++ continue_square_pattern.cpp
@@ -23,18 +23,15 @@ using namespace std;
 int main(){
     int i,j;
      int n;
-     cout<<"Enter n value :";
+     cout<<"Enter n value : ";
      cin>>n;
      char ch='A';
      for(int i=0;i<n;i++){
       for(int j=0;j<n;j++){
-            if(i==0 && j==0){
                  cout<<ch<<" ";
-                 ch++;
+                 ch=ch+1;
             }
       }
       cout<<endl;
-     }
-     cout<<"The last character after the pattern : "<<endl;
-     return 0
+     return 0;
 }
