208710e229fe99aa848b558fb83a0156ff7a93de
Nisha R S <nisha.s@s.amity.edu>
2026-02-27 04:57:14 +0530

    completed square pattern

--- square_pattern.cpp
+++ square_pattern.cpp
@@ -3,11 +3,12 @@ using namespace std;
 
 int main (){
     // int i;
-    int n=5;
+    int n=3;
     // cout<<"Enter n value : ";
     // cin>>n;
+    char ch='A';
     for(int i=0;i<n;i++){ //outer loop
-        char ch='A';
+       
         for(int j=0;j<n;j++){ //inner loop
             cout<<ch<<" ";
             ch=ch+1; //65+1=66 -> 'B'
