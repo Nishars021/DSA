f3b24eec7f49115d8e63c3e126d079fc90477677
Nisha R S <nisha.s@s.amity.edu>
2026-02-27 05:14:10 +0530

    started with triangle pattern

--- triangle pattern.cpp
+++ triangle pattern.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 
 using namespace std
+int main(){
 int i,j;
 int n=3;
 
-for(i=1;i<=n;i++){ //Outer loop
-    
+for(int i=0;i<n;i++){ //Outer loop
+    for(int j=0;j<i+1;j++){ //Inner loop
+        cout<<" * ";
+    }
+    cout<<endl;
+}
+    return 0;
 }
