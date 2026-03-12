529c6e537d9964819c3af08fb26db3c6e77556ba
Nisha R S <nisha.s@s.amity.edu>
2026-03-13 05:05:41 +0530

    Performed Reverse Triangle pattern

--- Reverse_Triangle_Pattern.cpp
+++ Reverse_Triangle_Pattern.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 using namespace std;
 int main(){
-int n=3;
-for(int i=0;i<=n;i++){
-    for(int j=1;j<=i;j++){
-        cout<<j;
+int n=4;
+for(int i=1;i<=n;i++){
+    for(int j=i;j>=1;j--){
+        cout<<j<<" ";
     }
     cout<<endl;
 }
