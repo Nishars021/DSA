f49a421ea1c70205b7d25794bdd0bceece0349ec
Nisha R S <nisha.s@s.amity.edu>
2026-03-12 20:31:57 +0530

    formed the pattern

--- triangle_pattern2.cpp
+++ triangle_pattern2.cpp
@@ -3,12 +3,12 @@ using namespace std;
 
 int main(){
     int n=4;
-    char ch='A';
-    for (int i=0;i<n;i++){ //Outer loop
-        for(int j=0;j<i+1;j++){ //Inner loop
-            cout<<ch<<" "; 
+    // char ch='A';
+    for (int i=1;i<=n;i++){ //Outer loop
+        for(int j=1;j<=i;j++){ //Inner loop
+            cout<<j;
         }
-        ch=ch+1; //A+1=B, B+1=C, C+1=D
+        // ch=ch+1; //A+1=B, B+1=C, C+1=D
         cout<<endl;
     }
     return 0;
