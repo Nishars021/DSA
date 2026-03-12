8b3c2828d2509ce13fdf9c39989ad529d4a2d246
Nisha R S <nisha.s@s.amity.edu>
2026-03-13 04:55:27 +0530

    Performing new pattern

--- triangle_pattern2.cpp
+++ triangle_pattern2.cpp
@@ -3,12 +3,12 @@ using namespace std;
 
 int main(){
     int n=4;
-    // char ch='A';
-    for (int i=n;i>=0;i--){ //Outer loop
-        for(int j=i;j>=0;j--){ //Inner loop
-            cout<<i; //Print the character and a space
+    char ch='A';
+    for (int i=0;i<=n;i++){ //Outer loop
+        for(int j=0;j<=i;j++){ //Inner loop
+            cout<<ch<<" "; //Print the character and a space
         }
-        // ch=ch+1; //A+1=B, B+1=C, C+1=D
+        ch=ch+1; //A+1=B, B+1=C, C+1=D
         cout<<endl;
     }
     return 0;
